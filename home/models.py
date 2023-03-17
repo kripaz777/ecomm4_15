90c79dd966050d5454131445c2a5fb19d7dd7be7
Aayush <aceraayush@gmail.com>
2023-03-17 16:37:17 +0545

    models created

--- home/models.py
+++ home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=300)
+    logo = models.CharField(max_length=300)
+    slug = models.CharField(max_length=300,unique=True)
+    def __str__(self):
+        return self.name
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=300)
+    logo = models.CharField(max_length=300)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    slug = models.CharField(max_length=300,unique=True)
+
+    def __str__(self):
+        return self.name
