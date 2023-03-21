455a3fed2bac1a1eb3d4e08a21f9cd6d7c344afd
Aayush <aceraayush@gmail.com>
2023-03-21 16:58:44 +0545

    templete

--- home/views.py
+++ home/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import View
+from .models import *
 # Create your views here.
 class Base(View):
     views = {}
 
 class HomeView(Base):
     def get(self,request):
-
-        return render(request,'index.html')
+        self.views['categories'] = Category.objects.all()
+        self.views['brands'] = Brand.objects.all()
+        self.views['ads'] = Ad.objects.all()
+        self.views['sliders'] = Slider.objects.all()
+        self.views['hots'] = Product.objects.filter(labels = 'hot')
+        self.views['news'] = Product.objects.filter(labels='new')
+        self.views['sale'] = Product.objects.filter(labels='sale')
+        return render(request,'index.html',self.views)
