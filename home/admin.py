cab168e173d98d82962659797de4629af51e9a21
Aayush <aceraayush@gmail.com>
2023-04-05 17:57:31 +0545

    api get post

--- home/admin.py
+++ home/admin.py
@@ -5,9 +5,15 @@ admin.site.register(Category)
 admin.site.register(SubCategory)
 admin.site.register(Brand)
 admin.site.register(Ad)
-admin.site.register(Product)
+# admin.site.register(Product)
 admin.site.register(Slider)
 admin.site.register(Customer)
 admin.site.register(Contact)
 admin.site.register(ProductReview)
 admin.site.register(Cart)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name","price","stock","labels","category","subcategory","brand")
+    search_fields = ["name","description"]
+    list_filter = ['stock','labels']
