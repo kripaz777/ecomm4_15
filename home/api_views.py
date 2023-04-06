eafea724dc072e4217e6e5d7b78c440611f03f02
Aayush <aceraayush@gmail.com>
2023-04-06 17:47:14 +0545

    API filter

--- home/api_views.py
+++ home/api_views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import  viewsets
 from .serializers import *
+from rest_framework import filters
+
 
 import django_filters.rest_framework
 from .serializers import *
@@ -14,4 +16,8 @@ class ProductViewSet(viewsets.ModelViewSet):
 class ProductListView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend,filters.SearchFilter,filters.OrderingFilter]
+    search_fields = ['name','description','specification']
+    ordering_fields = ['id', 'name','price','discounted_price']
+    filterset_fields = ['category', 'stock','subcategory','brand','labels']
+
