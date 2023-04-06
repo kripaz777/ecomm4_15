7a3dc416c085ef49f48c9e376305406daecdaee6
Aayush <aceraayush@gmail.com>
2023-04-06 17:27:57 +0545

    API filter

--- home/api_urls.py
+++ home/api_urls.py
@@ -9,5 +9,5 @@ router.register(r'products', ProductViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-
+    path('product_filter', ProductListView.as_view(), name='product_filter'),
 ]
--- home/api_views.py
+++ home/api_views.py
@@ -1,7 +1,17 @@
 from django.contrib.auth.models import User
 from rest_framework import  viewsets
 from .serializers import *
+
+import django_filters.rest_framework
+from .serializers import *
+from rest_framework import generics
 # ViewSets define the view behavior.
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+class ProductListView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
